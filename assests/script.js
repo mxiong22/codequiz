dd3551982ad6dc19f18a2f92687636f2facdc22b
moony <moonyxiong@gmail.com>
2022-09-23 20:01:38 -0500

    started javascript, and html with assigning vars to starting conatiners

---
+++ assests/script.js
@@ -0,0 +1,33 @@
+var startcontainer = document.querySelector('.startcontainer');
+var startbutton = document.querySelector('.startbutton');
+var questioncontainer = document.querySelector('.questioncontainer');
+var highscorecontainer = document.querySelector('.highscorecontainer');
+var questiontext =document.querySelector ('.questiontext');
+var time= 10; 
+
+questioncontainer.style.display='none';
+highscorecontainer.style.display='none';
+
+
+startbutton.addEventListener('click', function(){
+startcontainer.style.display='none';
+//use style.display='block' to show first display='none' resembling a new page
+questioncontainer.style.display='block';
+controlTimer()
+
+questiontext.textContent="What is your name?"
+
+})
+
+function controlTimer(){
+    var countDown= setInterval(function(){
+        time--
+        console.log(time)
+        if (time == 0) {
+            clearInterval(countDown)
+        }
+    },1000)
+    
+
+
+}
